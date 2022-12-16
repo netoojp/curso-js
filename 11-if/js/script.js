d8514d6dceac8e362ae1b5bd5e6c2bffa9ca4b79
netoojp <jjoaopedronetoo@gmail.com>
2022-12-16 16:13:46 -0300

    segundo commit

---
+++ 11-if/js/script.js
@@ -0,0 +1,18 @@
+
+var idade = 16;
+var idadeMinima = 18;
+
+console.log("antes do if")
+
+if (idade > idadeMinima) {
+    console.log("pode fazer carteira de habilitação");
+}
+
+if (idade > 15) {
+    console.log("precisa esperar três anos ainda");
+}
+
+console.log("depois do if")
+
+
+
---
+++ 12-op-comparaçao/js/script.js
@@ -0,0 +1,37 @@
+var idade = 15;
+var possuiCarro = 1;
+
+
+//maior ou igual a 
+if(idade >= 18) {
+  console.log("O usuário pode fazer a carteira");
+}
+
+//menor ou igual a
+if(idade <= 17) {
+  console.log("O usuário não pode fazer a carteira");
+}
+
+if(possuiCarro) {
+  console.log("O usuário já pode andar de carro");
+}
+
+var nome = "Matheus";
+
+//igualdade-comparação
+if(nome == "Matheus") {
+  console.log("O seu nome é Matheus");
+}
+
+//diferença-se for diferente
+if(nome != "Matheus") {
+  console.log("O nome não é Matheus");
+}
+
+if(20 > 100) {
+  console.log("Passou");
+}
+
+if(100 < 20) {
+  console.log("Passou 2")
+}
