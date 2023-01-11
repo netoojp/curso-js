6a7415b470f91cd18cc6d1a83917a828b8226b98
netoojp <jjoaopedronetoo@gmail.com>
2023-01-11 15:32:15 -0200

    sétimo commit

---
+++ 49-keyndown-keyup/js/script.js
@@ -0,0 +1,17 @@
+//keydown
+document.addEventListener("keydown", function(event){
+
+  console.log(event.key);
+
+  if(event.key === "Enter"){
+    console.log("Apertou Enter");
+  }
+});
+
+//keyup
+document.addEventListener("keyup", function(event){
+
+  if(event.key === "Enter"){
+    console.log("Soltou Enter");
+  }
+});
